bd41c6d1b3789dcf83f939b5dec00fd525c01815
anwar khawli <anwarkhawle@gmail.com>
2023-09-22 16:39:30 +0300

    Let's complicate the API

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express")
 const app = express()
 app.get("/test", (req,res)=>{
-res.json({status:200, message:"ok"})
+    res.status(200).json({status:200, message:"ok"})
 })
 app.get("/time",(req,res)=>{
-const data = new Date()
-const hours = data.getHours()
-var minutes = data.getMinutes();
-res.json({status:200, message:`${hours}:${minutes}`})
+    const data = new Date()
+    const hours = data.getHours()
+    var minutes = data.getMinutes();
+    res.status(200).json({status:200, message:`${hours}:${minutes}`})
+})
+app.get("/hello/:id?",(req,res)=>{
+    let id = req.params.id
+    id?res.json({status:200, message:`Hello, ${id}`}):
+    res.status(200).json({status:200, message:"Hello!"})
+})
+
+app.get("/search",(req,res)=>{
+    (req.query.s != undefined)?
+    res.status(200).json({status:200, message:"ok", data:`${req.query.s}`}):
+    res.status(500).json({status:500, error:true, message:"you have to provide a search"})
 })
 app.listen(3000)
